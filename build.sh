60eeccb12c3fdebc5fa36fe129f73e57cb95a9bb
PythonDecorator <okpeamos.ao@gmail.com>
2023-11-07 23:40:18 +0100

    Configure pyproject.toml to make use of python 3.8.1

--- build.sh
+++ build.sh
@@ -2,17 +2,9 @@
 # exit on error
 set -o errexit
 
-echo "Installing the latest version of poetry..."
-
-pip install --upgrade pip
-
 pip install poetry==1.5.1
 
 python -m poetry install
-
-python -m pip install --upgrade pip
-
-pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
